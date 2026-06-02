5af947242d56170e3aa0ba741cf7f143ea69efc1
Åke Strandberg <ake@strandberg.eu>
2026-06-02 11:15:10 +0000

    Touchup

--- pyaqify/__init__.py
+++ pyaqify/__init__.py
@@ -1,6 +1,7 @@
 """Library for Miele integration with Home Assistant."""
 
-from .const import *  # noqa: F403
-from .const import VERSION as __version__  # noqa: F401
+from .const import VERSION as __version__
 from .model import *  # noqa: F403
-from .pyaqvify import *  # noqa: F403
+from .pyaqvify import AqvifyAPI, AqvifyAuthException
+
+__all__ = ["AqvifyAPI", "AqvifyAuthException", "__version__"]
